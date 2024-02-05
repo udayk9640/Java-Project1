d89b8e9d711793cb51097bf7a41ed58b902eb313
udayk9640 <155454784+udayk9640@users.noreply.github.com>
2024-02-05 20:48:57 +0530

    Add files via upload

---
+++ Payments.java
@@ -0,0 +1,25 @@
+ class Payments {
+	 int x = 10;
+        
+	public static void main(String[] args) {
+		
+     int x = 20;
+     x = 30;
+      System.out.println(x);
+     
+      Payments obj = new Payments();
+      obj.testMethod();
+      }
+	void testMethod()
+	{
+		this.x = 40;
+		
+		
+		System.out.println(this.x);
+		
+		
+		
+	}
+	
+}
+ 
