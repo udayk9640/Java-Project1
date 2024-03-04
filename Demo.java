ad826b876551ed52732581a6be5c4e3c62c59113
udayk9640 <155454784+udayk9640@users.noreply.github.com>
2024-03-04 20:21:22 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,13 +1,3 @@
 
-class Demo{
-   static int val_1;
-   int val_2;
-   static{
-      val_1 = 67;
-      System.out.println("The static block has been called.");
-   }
-   {
-	   System.out.println("Hello");
-   }
-}
+
 
