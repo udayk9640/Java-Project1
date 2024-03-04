ec0688d36459e8ae492ababc6977e2f90509380b
udayk9640 <155454784+udayk9640@users.noreply.github.com>
2024-03-04 20:23:05 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,3 +1,29 @@
 
 
+class Demo{
+   static int val_1;
+   int val_2;
+   static{
+      val_1 = 67;
+      System.out.println("The static block has been called.");
+   }
+   static{
+	      val_1 = 68;
+	      System.out.println("The static block has been called twice.");
+	   }
+   static{
+	      val_1 = 68;
+	      System.out.println("The static block has been called thrice.");
+	   }
+   
+   {
+	   System.out.println("Hello");
+   }
+}
+
+
+
+
+
+
 
