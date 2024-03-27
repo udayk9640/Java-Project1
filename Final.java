1d2dcd46b405ab756e9891baa58c8643f727bb30
udayk9640 <155454784+udayk9640@users.noreply.github.com>
2024-03-27 20:43:01 +0530

    Add files via upload
    final file added

---
+++ Final.java
@@ -0,0 +1,14 @@
+
+public class Final {
+	public static void main(String args[]) {
+		final int x = 10;
+		//x = 20;
+		System.out.println(x);
+		final StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+
+        System.out.println("The value of sb: " + sb);
+		
+	}
+
+}
